f5fe25d32877c87db30dfadd7bb2528bf3df739b
arch1998 <57918927+arch1998@users.noreply.github.com>
2021-03-17 20:44:02 +0530

    Add files via upload

---
+++ info.sql
@@ -0,0 +1,7 @@
+create table student_info(Name char , Roll int , Address varchar(20),Mobno varchar(10),PAN_number varchar(20));
+insert into student_info values('ARCHANA',2012,'Nashik',7689564738 ,'NXYT23488');
+insert into student_info values('SIMARAN',2013,'NAGPUR',9087967545 ,'YTRE23134');
+insert into student_info values('SHEWTA',2014,'AHMDNAGAR',9087658490 ,'MNHE3456');
+insert into student_info values('RISHI',2015,'Pune',78956474675,'KLEIB9876');
+insert into student_info values('KRISHNA',2016,'MUMBAI',92349098787 ,'ZSWQT5982');
+
